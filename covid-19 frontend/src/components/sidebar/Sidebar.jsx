d846b5045fa8a8d01b3d764a8199417c21971e1c
rawbeen72 <75427075+rawbeen72@users.noreply.github.com>
2022-02-11 21:24:37 +0545

    Update Sidebar.jsx

--- covid-19 frontend/src/components/sidebar/Sidebar.jsx
+++ covid-19 frontend/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Card from '../Cards/Card'
 
-const Sidebar = ({data}) => {
+const Sidebar = ({data, globalCases}) => {
   return (
    <div className="md:w-80 lg:h-full h-1/2 shadow-lg absolute grid gap-1 lg:mx-3 mx-0 my-2 w-full">
 
-              <Card className="bg-blue-600 h-2 mt-3" text={"Total number of infected cases"} title={"Total Infected Cases"} date={new Date(data.lastUpdate).toDateString()} cases={data.confirmed} />
-              <Card className="bg-green-500 h-2 mt-3" text={"Total number of recovered cases "} title={"Recovered Cases"} date={new Date(data.lastUpdate).toDateString()} cases={data.recovered} />
-              <Card className="bg-orange-600 h-2 mt-3" text={"Number of critical cases "} title={"Critical Cases"} date={new Date(data.lastUpdate).toDateString()} cases={data.critical} />
-              <Card  className="bg-red-600 h-2 mt-3" text={"Total number of deaths"} title={"Total Deaths"} date={new Date(data.lastUpdate).toDateString()} cases={data.deaths} />
+              <Card className="bg-blue-600 h-2 mt-3" text={"Total number of infected cases"} title={"Total Infected Cases"} date={new Date(globalCases.date).toDateString()} cases={data.confirmed} />
+              <Card className="bg-green-500 h-2 mt-3" text={"Total number of recovered cases "} title={"Recovered Cases"} date={new Date(globalCases.date).toDateString()} cases={data.recovered} />
+              <Card className="bg-orange-600 h-2 mt-3" text={"Number of critical cases "} title={"Critical Cases"} date={new Date(globalCases.date).toDateString()} cases={data.critical} />
+              <Card  className="bg-red-600 h-2 mt-3" text={"Total number of deaths"} title={"Total Deaths"} date={new Date(globalCases.date).toDateString()} cases={data.deaths} />
           
 </div>
   )
